ffbb92413ba2b754764c30ceabd35f63681a8ae1
Patrick Mims <patrickkevinmims@gmail.com>
2020-12-13 20:18:38 -0800

    Latest Changes

--- ch11/projects/ex1/src/main.cc
+++ ch11/projects/ex1/src/main.cc
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+void showRelease(const movie_t *release)
+{
+  cout << "The famous director: " << release->director << endl;
+}
+
 void information(movie_t film)
 {
   cout << "Title: " << film.title << "\nDirector: " << film.director << "\nYear Released: " << film.released << endl;
@@ -11,6 +16,13 @@ void information(movie_t film)
 int main(void)
 {
   movie_t movie = {"Training Day", "Michael Jones", 2001, 89};
+  movie_t *ptrShow = 0;
+
+  ptrShow = &movie;
+
+  cout << "First Year Released: " << ptrShow->released << endl;
+
+  showRelease(ptrShow);
 
   information(movie);
 
