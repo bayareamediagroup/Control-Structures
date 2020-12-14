ab41a8f76cf9d49312af065806bfbf00801cf1a2
Patrick Mims <patrickkevinmims@gmail.com>
2020-12-13 19:43:46 -0800

    Latest Completed Files

---
+++ ch11/projects/ex1/inc/movie.h
@@ -0,0 +1,14 @@
+#ifndef _EX1_
+#define _EX1_
+
+typedef struct Movie_t
+{
+    char *title;
+    char *director;
+    int released;
+    int running_time;
+} movie_t;
+
+void information();
+
+#endif
---
+++ ch11/projects/ex1/src/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "movie.h"
+
+using namespace std;
+
+int main(void)
+{
+  movie_t movie;
+
+  return 0;
+}
+
+/*
+ * Movie Data
+ * Write a program that uses a structure named MovieData to store the following information about a movie.
+ *
+ * Title
+ * Director
+ * Year Released
+ * Running Time (in minutes)
+ *
+ * The program should create two MoveData variables, store values in their members,
+ * and pass each one, in turn, to a function that displays the information about the movie
+ * in a clealy formated manner.
+ * */
