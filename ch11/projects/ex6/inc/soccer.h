6dc6f3203ac0d801225b3b48a1283a88e6db7be2
Patrick Mims <patrickkevinmims@gmail.com>
2020-12-20 14:52:21 -0800

    init

---
+++ ch11/projects/ex6/inc/soccer.h
@@ -0,0 +1,11 @@
+#ifndef SOCCER_H
+#define SOCCER_H
+
+typedef struct
+{
+  char *name;
+  int number;
+  int points;
+} soccer_t;
+
+#endif
---
+++ ch11/projects/ex6/src/main.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+
+#include "soccer.h"
+
+#define SIZE 12
+
+using namespace std;
+
+int main(void)
+{
+  soccer_t soccer[12];
+
+
+
+  return 0;
+}
