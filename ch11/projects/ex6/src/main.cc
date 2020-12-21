5fee153f8d231c41b2b669923fbb8dd86c9bc69f
Patrick Mims <patrickkevinmims@gmail.com>
2020-12-21 15:28:15 -0800

    Added a function that takes the struct as an argument

--- ch11/projects/ex6/src/main.cc
+++ ch11/projects/ex6/src/main.cc
@@ -1,35 +1,51 @@
-#include <iostream>
-#include <cstring>
 #include <cstdio>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
 
 #include "soccer.h"
 
-#define SIZE 2
+#define SIZE 5
 
 using namespace std;
+
+static void display(soccer_t *l)
+{
+  int i;
+  for(i = 0; i < SIZE; i++)
+  {
+    cout << l[i].name << setw(10) << l[i].number << setw(10) << l[i].points << endl;
+  }
+}
 
 static void collect_scores(soccer_t *s, int i)
 {
   cout << "Enter Name, Number and Points scored per player " << i << endl;
-  getline(cin, s->fname);
 
-  cout << "Number: " << endl;
+  cout << "Name: ";
+  cin.ignore();
+  getline(cin, s->name);
+
+  cout << "Number: ";
   cin >> s->number;
 
-  cout << "Points Scored: " << endl;
+  cout << "Points Scored: ";
   cin >> s->points;
+
   cout << endl;
 }
 
 int main(void)
 {
-  int cnt = 1;
+  int cnt = 0;
   soccer_t soccer[SIZE];
 
   do {
     collect_scores(&soccer[cnt], cnt);
     cnt++;
-  } while(cnt <= SIZE);
+  } while(cnt < SIZE);
+
+  display(soccer);
 
   return 0;
 }
