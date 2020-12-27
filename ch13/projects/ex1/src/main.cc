542b90893c007b33de5d1bb3db05f43bbeaedc3e
Patrick Mims <patrickkevinmims@gmail.com>
2020-12-26 18:21:06 -0800

    Modified CMakeLists.txt to support C++17

--- ch13/projects/ex1/src/main.cc
+++ ch13/projects/ex1/src/main.cc
@@ -9,12 +9,19 @@ using namespace std;
 int main(void)
 {
   CDate cdate;
+  CDate *dte = nullptr;
+
+  dte = &cdate;
 
   cdate.setDay(26);
   cdate.setMonth(12);
   cdate.setYear(2021);
 
   cout << cdate.getDay() << "/"  << cdate.getMonth() << "/" << cdate.getYear() << endl;
+
+  dte->setDay(9);
+
+  cout << "Your Birthday is: " << dte->getDay() << endl;
 
   return 0;
 }
