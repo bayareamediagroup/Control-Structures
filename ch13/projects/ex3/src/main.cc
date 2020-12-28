4ca4acc4e02af7df6027296c90b2dbd0ef9f3ebc
Patrick Mims <patrickkevinmims@gmail.com>
2020-12-28 09:33:31 -0800

    added control_speed() function

--- ch13/projects/ex3/src/main.cc
+++ ch13/projects/ex3/src/main.cc
@@ -5,21 +5,40 @@
 
 using namespace std;
 
+void control_speed(Car &c)
+{
+    cout << "car speed: " <<  c.getSpeed() << endl;
+}
+
 int main(int argc, char **argv)
 {
     Car car;
     Car *c = nullptr;
     c = &car;
 
-    cout << "Speed: " << c->getSpeed() << endl;
-    cout << "Year: " << c->getYear() << endl;
-    cout << "Accelerate 5 miles per hour 1: " << endl;
-    c->accelerate();
-    cout << "Speed: " << c->getSpeed() << endl;
+    int i = 0, j = 0;
 
-    cout << "Accelerate 5 miles per hour 2: " << endl;
-    c->accelerate();
-    cout << "Speed: " << c->getSpeed() << endl;
+    cout << "The Car is idle at: " << c->getSpeed() << endl;
+
+    while (i < 5)
+    {
+      c->accelerate();
+      cout << "Accelerating..." << c->getSpeed() << endl;
+      i += 1;
+    }
+
+    cout << "The Car is crusing at: " << c->getSpeed() << endl;
+
+    while(j < 5)
+    {
+      cout << "Braking..." << c->getSpeed() << endl;
+      c->brake();
+      j += 1;
+    }
+
+    cout << "The Car is now at: " << c->getSpeed() << endl;
+
+    control_speed(car);
 
     return 0;
 }
