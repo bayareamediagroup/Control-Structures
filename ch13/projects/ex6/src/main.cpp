8d85e8d96530376658fa96f7dbdb2dab80f80d1d
Patrick Mims <patrickkevinmims@gmail.com>
2021-01-06 20:02:09 -0800

    Project Init

---
+++ ch13/projects/ex6/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+
+#include "inventory.hpp"
+
+using namespace std;
+
+int main(void)
+{
+  Inventory *inventory = nullptr;
+  inventory = new Inventory();
+
+  return 0;
+}
