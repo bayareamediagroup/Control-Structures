e3231411bd607094a984d68b825cf512147edb1f
Patrick Mims <patrickkevinmims@gmail.com>
2021-01-06 19:10:09 -0800

    Finished Project

--- ch13/projects/ex5/src/main.cpp
+++ ch13/projects/ex5/src/main.cpp
@@ -3,14 +3,27 @@
 
 #include "retailItem.hpp"
 
+void createItem(const RetailItem *item, string *d, double p, int u)
+{
+  RetailItem *retailItem1 = nullptr;
+}
+
 int main(void)
 {
-  RetailItem *retailItem = nullptr;
-  retailItem = new RetailItem("Coat", 32.00, 4);
+  RetailItem *retailItem1 = nullptr;
 
-  cout << "Description: " << retailItem->getDescription() << endl;
-  cout << "Price: " << retailItem->getPrice() << endl;
-  cout << "Units On Hand: " << retailItem->getUnitsOnHand() << endl;
+  RetailItem *retailItem2 = nullptr;
+  RetailItem *retailItem3 = nullptr;
 
+  retailItem1 = new RetailItem("Coat", 52.21, 4);
+
+  retailItem1->setDescription("Shirt");
+  retailItem1->setPrice(88.83);
+  retailItem1->setUnitsOnHand(6);
+
+  cout << "Description"
+       << "Price"
+       << "Units" << endl;
+  cout << " " << retailItem1->getDescription() << " " << retailItem1->getPrice() << " " << retailItem1->getUnitsOnHand() << endl;
   return 0;
 }
