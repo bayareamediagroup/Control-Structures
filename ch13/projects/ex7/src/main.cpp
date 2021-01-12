87ae4ab6376d1f328f0940f022323efe5a5d18a5
Patrick Mims <patrickkevinmims@gmail.com>
2021-01-11 18:33:38 -0800

    Finished Challenge

--- ch13/projects/ex7/src/main.cpp
+++ ch13/projects/ex7/src/main.cpp
@@ -8,12 +8,22 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+  int i = 0;
+  double tc[3] = {0};
   Scores scores;
   Scores *sc = nullptr;
 
   sc = &scores;
 
   sc->setAverageTestScores(95.4, 78.9, 88.3);
+
+  do {
+    cout << "Test Score " << i << endl;
+    cin >> tc[i];
+    i++;
+  } while(i < 3);
+
+  sc->setAverageTestScores(tc[0], tc[1], tc[2]);
 
   cout << "Average Scores: " << sc->getAverageTestScores() << endl;
 
