e3f491bd8776e476835efcb0e7e744fe4445cfab
Patrick Mims <patrickkevinmims@gmail.com>
2021-01-11 18:42:51 -0800

    circle.hpp init

--- ch13/projects/ex8/inc/circle.hpp
+++ ch13/projects/ex8/inc/circle.hpp
@@ -3,11 +3,18 @@
 
 class Circle
 {
+  private:
+    double pi;
+    double radius;
 
-
-
-
-
+  public:
+    Circle();
+    Circle(double);
+    void setRadius(double);
+    double getArea() const;
+    double getRadius() const;
+    double getDiameter() const;
+    double getCircumference() const;
 };
 
 #endif
